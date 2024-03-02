088df319bedf19199d8b933997eadff1902eacc5
Ali <soyseyitali23@gmail.com>
2024-03-02 15:58:38 +0300

    created MVC layer and efcore implementations

---
+++ Business/Abstract/IDocumentService.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IDocumentService
+    {
+        List<Document> GetAll();
+
+        Document GetById(int id);
+
+        void Add(Document entity);
+
+        void Delete(Document document);
+
+        void Update(Document document);
+    }
+}
---
+++ Business/Concrete/DocumentManager.cs
@@ -0,0 +1,46 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class DocumentManager : IDocumentService
+    {
+        IDocumentDal _documentDal;
+
+        public DocumentManager(IDocumentDal documentDal)
+        {
+            _documentDal = documentDal;
+        }
+
+        public void Add(Document entity)
+        {
+            _documentDal.Add(entity);
+        }
+
+        public void Delete(Document document)
+        {
+            _documentDal.Delete(document);
+        }
+
+        public List<Document> GetAll()
+        {
+            return _documentDal.GetAll();
+        }
+
+        public Document GetById(int id)
+        {
+            return _documentDal.Get(i => i.Id == id);
+        }
+
+        public void Update(Document document)
+        {
+            _documentDal.Update(document);
+        }
+    }
+}
